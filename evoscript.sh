f4a9604f4fa783f12da4b2fe856861202585e985
nyxalune <akfylmz350@gmail.com>
2025-09-30 18:45:54 +0300

    Add Evolution Script.

---
+++ evoscript.sh
@@ -0,0 +1,24 @@
+#First lets setup environment
+
+git clone https://github.com/akhilnarang/scripts &&  cd scripts &&  bash setup/android_build_env.sh && echo Islem Tamam.
+
+#Then Lets sync
+
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+ && repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+ && echo Islem Tamam
+
+#Lets sync our trees too!
+
+git clone https://github.com/nyxalune/device_nothing_Spacewar.git
+ -b bka device/nothing/Spacewar && git clone https://github.com/nyxalune/kernel_nothing_sm7325.git
+ kernel/nothing/sm7325 && https://github.com/nyxalune/vendor_nothing_Spacewar.git
+-b bka vendor/nothing/Spacewar && git clone https://github.com/nyxalune/hardware_nothing.git
+ hardware/nothing && git clone https://github.com/nyxalune/hardware_dolby.git
+ -b sony hardware/dolby && echo Islem Tamam
+
+#Now for my final trick, lets build.
+
+source build/envsetup.sh && lunch lineage_Spacewar-userdebug && m evolution
+
+
